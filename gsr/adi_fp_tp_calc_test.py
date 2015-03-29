01f5442c0185fbf8458ef91f0d8b3a2b428f96a4
Adityan Ajit <adityana39@gmail.com>
2015-03-29 11:02:06 -0400

    Updates to fp_tp file, not runnable yet

--- gsr/adi_fp_tp_calc_test.py
+++ gsr/adi_fp_tp_calc_test.py
@@ -2,55 +2,52 @@ import json
 import datetime
 import os
 
+#Text file containing [city, date, score]
 
+pred = [] # value to hold city, dt, score 
+
+# Proc equivalent 
+lines = open(" ", "r").readlines() #ADD: Open file with [city, date, score] (add event type as well?) 
+for line in lines:
+	items = line.split()
+	city = items[0] 
+	dt = datetime.datetime.strptime(items[1], "%Y-%m-%d")
+	score = float(items[2])
+	
+	
+	
+	gsr_file = "" #ADD: where the gsr file is defined, gsr file should have id, dt, et 
+	pred = sorted(pred, key = lambda item: item[0])
+	
+	gsr = dict()
+	
+	for line in open(gsr_file).readlines():
+		items = line.split()
+		ci = items[1] 
+		id = items[4]
+		dt = items[5] 
+		
+		if gsr.has_key(ci, id):
+			gsr[(ci, id)][dt] = 0
+		else:
+			gsr[(ci,id)][dt] = 0
+		
+		tprs = []
+    	fps = []
+    	for i in range(1, 300):
+        	cur_pred = pred[:i] #cur_pred takes predictions to a certain range 
+        	tpr, fp = tpr_fp(cur_pred, gsr) #proc calls tpr_fp here to return tpr fp 
+        	tprs.append(tpr)
+        	fps.append(fp)
+
+    	return tprs, fps
+
+# TP/FP Equivalent 	
+
+	
 
 
 """
-def proc(predfolder, co, method):
-	#Predfolder is defined as predfolder = '/raid/home/zhoubj/KDD-Code/KDD-NPSS/realDataResult/{0}/civilUnrest/{1}/PValue'.format(method, co)
-    #Co is the country (iterated through a list)
-    #Method is from the list of methods - only one is necessary for this 
-    
-    files = [f for f in os.listdir(predfolder)]
-    pred = []
-    for f in files:
-        for line in open(os.path.join(predfolder, f)).readlines():
-            if line.find('null') < 0 and len(line) > 5:
-                items = line.split()
-                id = int(items[0])
-                score = float(items[1])
-                dt = datetime.datetime.strptime(items[2], "%Y-%m-%d")
-                pred.append((score, id, dt))
-    folder = '/raid/home/zhoubj/KDD-Code/KDD-NPSS/realDataSet/CivilUnrest/{0}'.format(co)
-
-    gsr_file = os.path.join(folder, 'gsr.txt')
-
-    pred = sorted(pred, key = lambda item: item[0] * -1)
-    
-    n = len(pred)
-
-    gsr = dict()
-    for line in open(gsr_file).readlines():
-        items = line.split()
-        id = int(items[0])
-        dt = datetime.datetime.strptime(items[1], "%Y-%m-%d")
-        if gsr.has_key(id):
-            gsr[id][dt] = 0
-        else:
-            gsr[id] = {dt: 0}
-
-    tprs = []
-    fps = []
-    for i in range(1, 300):
-        cur_pred = pred[:i]
-        tpr, fp = tpr_fp(cur_pred, gsr) #proc calls tpr, fp here 
-        tprs.append(tpr)
-        fps.append(fp)
-
-    return tprs, fps
-
-
-# This is a function that determines the precision (True positive rate) 
 def tpr_fp(cur_pred, gsr):
     fp = 0
     tp = 0
@@ -58,7 +55,8 @@ def tpr_fp(cur_pred, gsr):
     n = 0
     for id, dts in gsr.items():
         n = n + len(dts)
-
+	
+	#g
     for score, id, dt in cur_pred:
         flag = 0
         if gsr.has_key(id) and gsr[id].has_key(dt):
@@ -76,6 +74,89 @@ def tpr_fp(cur_pred, gsr):
             if gsr.has_key(id) and gsr[id].has_key(nd):
                 data[(id, nd)] = 1
                 flag = 1
+        # If there are no flags (predictions or forecasts, adds one to the false positive rating) 
+        if flag == 0:
+            fp = fp + 1
+    tp = len(data)
+    return tp / (n * 1.0), fp
+
+
+
+
+def proc(predfolder, co, method):
+	#Predfolder is defined as predfolder = '/raid/home/zhoubj/KDD-Code/KDD-NPSS/realDataResult/{0}/civilUnrest/{1}/PValue'.format(method, co)
+    #Co is the country (iterated through a list)
+    #Method is from the list of methods - only one is necessary for this 
+    
+    files = [f for f in os.listdir(predfolder)]
+    pred = [] # value to hold score, id, date time 
+    for f in files:
+        for line in open(os.path.join(predfolder, f)).readlines():
+            if line.find('null') < 0 and len(line) > 5:
+                items = line.split()
+                id = int(items[0])
+                score = float(items[1])
+                dt = datetime.datetime.strptime(items[2], "%Y-%m-%d")
+                pred.append((score, id, dt))
+    folder = '/raid/home/zhoubj/KDD-Code/KDD-NPSS/realDataSet/CivilUnrest/{0}'.format(co)
+
+    gsr_file = os.path.join(folder, 'gsr.txt')
+
+    pred = sorted(pred, key = lambda item: item[0] * -1)
+    
+    n = len(pred)
+
+    gsr = dict()
+    
+    # Iterates through each file of the gsr and adds an event to the dictionary for each warning
+    for line in open(gsr_file).readlines():
+        items = line.split()
+        id = int(items[0])
+        dt = datetime.datetime.strptime(items[1], "%Y-%m-%d")
+        if gsr.has_key(id):
+            gsr[id][dt] = 0
+        else:
+            gsr[id] = {dt: 0}
+
+    tprs = []
+    fps = []
+    for i in range(1, 300):
+        cur_pred = pred[:i] #cur_pred takes predictions to a certain range 
+        tpr, fp = tpr_fp(cur_pred, gsr) #proc calls tpr_fp here to return tpr fp 
+        tprs.append(tpr)
+        fps.append(fp)
+
+    return tprs, fps
+
+
+# This is a function that determines the precision (True positive rate) 
+def tpr_fp(cur_pred, gsr):
+    fp = 0
+    tp = 0
+    data = dict()
+    n = 0
+    for id, dts in gsr.items():
+        n = n + len(dts)
+	
+	#g
+    for score, id, dt in cur_pred:
+        flag = 0
+        if gsr.has_key(id) and gsr[id].has_key(dt):
+            data[(id, dt)] = 1
+            flag = 1
+        nd = dt
+        for i in range(7):
+            nd = nd + datetime.timedelta(days=1)
+            if gsr.has_key(id) and gsr[id].has_key(nd):
+                data[(id, nd)] = 1
+                flag = 1
+        nd = dt
+        for i in range(7):
+            nd = nd - datetime.timedelta(days=1)
+            if gsr.has_key(id) and gsr[id].has_key(nd):
+                data[(id, nd)] = 1
+                flag = 1
+        # If there are no flags (predictions or forecasts, adds one to the false positive rating) 
         if flag == 0:
             fp = fp + 1
     tp = len(data)
