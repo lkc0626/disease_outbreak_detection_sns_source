b259c2edbb67763199d81c69fc1f292cd41da249
Ziyun Zeng <zyzeng1412@gmail.com>
2015-04-05 12:02:59 -0400

    Combinetwitter for windows

---
+++ Combinetwitter_windows/pyfiles/adi_fp_tp_calc_test.py
@@ -0,0 +1,190 @@
+import json
+import datetime
+import os
+import inspect
+import dateutil
+import matplotlib.pyplot as plt
+
+
+
+#File to take warning files from subgraph files 
+def makeWarningFile(path):
+	warningPath = os.path.join(path+'/input', 'warning_file.txt')	
+	warning_file = open(warningPath, 'a')
+	warning_file.seek(0)
+	warning_file.truncate()	 # Deletes contents in warning file from previous use 
+		
+	for filename in os.listdir(path+'/output/subgraph'):
+		dateobj = datetime.datetime.strptime(filename[0:10],'%Y-%m-%d').date()		
+		file_path = os.path.join(path+'/output/subgraph', filename)
+		items = open(file_path).read().split()
+		#print items
+		score = items[-1]
+		for id in range(len(items)-1):							
+			warning_file.write('{0} {1} {2}\n'.format(items[id], filename[0:10], score))
+
+#Makes a list of predictions based on the warning file that Embers generates 
+
+def makePredictionList(warning_file, cutoff):
+	pred = [] # value to hold city, dt, score 
+	# Proc equivalent 
+	lines = open(warning_file, "r").readlines() #ADD: Open file with [id, date, score] 
+	for line in lines:
+		items = line.split()
+		id = items[0] 
+		dt = datetime.datetime.strptime(items[1], "%Y-%m-%d")
+		score = float(items[2])
+		if score >= cutoff:
+			pred.append((id, dt, score)) #adding id, dt, score values 
+			
+	pred = sorted(pred, key = lambda item: item[2], reverse=True) #sorts pred value based on score
+	#print pred
+	#print "Predictions: "+str(len(pred))
+	return pred 
+
+			
+#Makes a gsr list based off the gsr_file 
+
+def makeGsrList(gsr_file):
+	gsr = dict() #Dictionary to hold gsr events from file 
+	
+	for line in open(gsr_file).readlines():
+		items = line.split()
+		id = items[0] 
+		dt = items[1] 
+		
+		if gsr.has_key(id):
+			gsr[id][dt] = 0
+		else:
+			gsr[id] = {dt: 0} 
+		
+	#print "GSR Events: "+str(len(gsr))
+	return gsr 
+			
+# TP/FP Equivalent 	
+
+#Formula for recall is total true events within two weeks / total true events 
+#Formula for true positive rate is true positive / true positive + false positive
+#Formula for false positive rate is false positive days / # days 
+
+def tpr_fp(pred, gsr, k):
+	fp = 0 # number of false positives 
+	tp = 0 # number of true positives 
+	data = dict() # used to hold all the true events 
+	n = dict() # number of events that happen within a 2 week window of all nodes 
+	i = 0 
+	for id, dt, score in pred:
+		if (i > k):
+			break
+		i += 1 
+		flag = 0
+		
+		# First if statement checks if there is an exact match between a gsr event and an EMBERS warning
+		if gsr.has_key(id) and gsr[id].has_key(dt):
+			data[(id,dt)] = 1
+			flag = 1
+			
+		# Second statement to check if there is  match in the two week window 
+		else: 
+			nd1 = nd2 = dt 
+			for i in range(7):
+				nd1 = nd1 + datetime.timedelta(days=1)
+				nd2 = nd2 - datetime.timedelta(days=1)
+				if gsr.has_key(id) and gsr[id].has_key(nd1):
+					data[(id,dt)] = 1
+					flag = 1
+					nd = nd1
+					break  
+				if gsr.has_key(id) and gsr[id].has_key(nd2):
+					data[(id,dt)] = 1
+					flag = 1 
+					nd = nd2
+					break 
+				
+		# Statement that increments the false positive 
+		if flag == 0:
+			fp = fp + 1
+
+		if flag == 1:
+			nd = dt - datetime.timedelta(days = 7)
+			for i in range(14):
+				nd = dt + datetime.timedelta(days = 1) 
+				if gsr[id].has_key(nd):
+					if not n.has_key(id):
+						n[id] = dt					
+				
+			
+
+
+	tp = len(data)
+	tpr = tp / len(pred)
+	fpr = fp / len(pred) 
+	if (len(n) != 0):
+		recall = tp / len(n) 
+		return tpr, fpr, recall 
+
+	return tpr, fpr, 0 # returns 0 for recall if it is not detected 
+
+
+
+	
+	
+def graph(tprList, fprList, recallList, filename):
+	
+	# Shows plot of tpr vs. fpr 
+	plt.plot(tprList, fprList, '-')
+	plt.xlabel('True Positive Rate')
+	plt.ylabel('False Positive Rate')
+	plt.show()
+	# Shows plot of tpr vs. recall 
+	if not (recallList[0] == 0): # only show if there is an element in recall, edit to make more robust
+		plt.plot(tprList, recallList, '-')
+		plt.xlabel('True Positive Rate')
+		plt.ylabel('False Positive Rate') 
+		plt.show()	
+
+def fp_tp_calc(cutoff, k, path): 
+	#os.chdir('..')
+	#path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
+	makeWarningFile(path)
+	pred = makePredictionList(path+'/input/warning_file.txt', cutoff) 
+	gsrPath = path+'/input/gsr_cutoff'
+		
+	for filename in os.listdir(gsrPath):
+		gsr = makeGsrList(os.path.join(gsrPath,filename))
+		tprList = []
+		fprList = []
+		recallList = []
+		for i in range(k):
+			#print i
+			tpr,fpr, recall = tpr_fp(pred, gsr, i)
+			tprList.append(tpr)
+			fprList.append(fpr)
+			recallList.append(recall)
+		graph(tprList, fprList, recallList, filename) 
+
+if __name__ == '__main__':
+	
+	path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
+	makeWarningFile(path)
+	cutoff = 0 # change this 
+	k = 50 # change this 
+	pred = makePredictionList(path+'input/warning_file.txt', cutoff) 
+	gsrPath = path+'/input/gsr_cutoff'
+	
+	
+		
+	for filename in os.listdir(gsrPath):
+		gsr = makeGsrList(os.path.join(gsrPath,filename))
+		tprList = []
+		fprList = []
+		recallList = []
+		for i in range(k):
+			#print i
+			tpr,fpr, recall = tpr_fp(pred, gsr, i)
+			tprList.append(tpr)
+			fprList.append(fpr)
+			recallList.append(recall)
+		graph(tprList, fprList, recallList, filename) 
+		
+
---
+++ Combinetwitter_windows/pyfiles/main.py
@@ -0,0 +1,52 @@
+import sys
+#from sys import gsr_proc
+from adi_gsr_test import gsr
+from geo_proc_twitter import geo_proc_twitter_f
+from twitter_calc_pvalues import  mainproc
+from getinfnpss import getinfnpss_f
+from adi_fp_tp_calc_test import fp_tp_calc
+import pickle
+import json
+import datetime
+import os
+def gdt(str):
+    return datetime.datetime.strptime(str, "%Y-%m-%d")
+
+#os.chdir('..')
+
+cos = [ 'chile']
+for co1 in cos:
+
+	event_Type='0313'
+	stdt  = gdt('2012-08-25')
+	cutdt = gdt('2012-08-27')
+	enddt = gdt('2012-10-29')
+
+	key_terms=['virus', 'epidemia', 'enfermos', 'hanta', 'viral', 'territorio', 'pneumonia', 'sangre', 'ratones', 'cardiopulmonar', 'vacuna', 'campos', 'provincial', 'hantavirus', 'tosse', 'nariz', 'estornudar', 'abdominal', 'lluvia', 'renal', 'paciente', 'transmissor', 'lixo', 'criaderos', 'respiratorias', 'manos', 'boca', 'rural', 'musculares', 'roedores']
+
+
+	#folder = '../input/raw/twitter-2012'
+
+	folder = os.path.abspath(os.path.pardir)+'/input/raw/twitter-2012'
+	path = os.path.abspath(os.path.pardir)
+
+	gsrfile1 = 'gsrFebruary_all.mjson'
+ 
+	gsr(co1,event_Type,gsrfile1, cutdt,enddt)
+
+	geo_proc_twitter_f(key_terms,folder, path,co1)
+
+	gsrfile=co1+'_'+event_Type+'_gsr.txt'
+
+	databaseName='{0}_twitter_cnt_data.txt'.format(co1)
+	mainproc(co1,gsrfile,databaseName,stdt,cutdt,enddt, path)
+
+
+	alpha_max = 1
+
+	graphfile=path+'/pyfiles/chile_k_25_graph.txt'
+	getinfnpss_f(alpha_max,graphfile, path)
+	cutoff_score=0
+	k = 50 
+	# Calculates fp/tp and draws graphs 
+	fp_tp_calc(cutoff_score, k, path)
